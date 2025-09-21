a701fa6c3ccb072189fa2ce3f088993d83798fd1
gwitcig2 <gwitcig2@huskers.unl.edu>
2025-09-20 19:40:04 -0500

    Established routing with react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,18 @@
 
-// import './App.css'
-import {useState} from "react";
-import SentenceViewer from "./readingComponents/SentenceViewer.tsx";
+import { Routes, Route } from "react-router-dom";
 
-type Sense = {
-  pos: string[];
-  meanings: string[];
-  misc: string[];
-  info: string[];
-  appliesToKanji: string[];
-  appliesToKana: string[];
-  _id: string;
-};
+import LoginPage from "./features/auth/LoginPage.tsx";
+import SignupPage from "./features/auth/SignupPage.tsx";
+import ReadingPage from "./features/reading/ReadingPage.tsx";
 
-type Token = {
-  surface_form: string;
-  basic_form: string;
-  reading: string;
-  senses?: Sense[];
-};
 function App() {
 
-  const [result, setResult] = useState<Token[] | null>(null);
-
-  const testReadingProcess = async () => {
-
-    const res = await fetch("http://localhost:5000/api/procedures/processReading", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" }
-      // body: JSON.stringify({ input })
-    });
-    const data = await res.json();
-    setResult(data.tokens);
-
-  };
-
   return (
-    <div className="flex flex-wrap flex-col gap-1 text-7x1 justify-center">
-      <div className="flex justify-center pb-2">
-        <button className="border-3 px-5 py-1" onClick={testReadingProcess}>Click to Test</button>
-      </div>
-
-
-      <SentenceViewer tokens={result} />
-    </div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/reading" element={<ReadingPage />} />
+      </Routes>
   )
 }
 
