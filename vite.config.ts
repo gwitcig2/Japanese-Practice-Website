3bf44854d8e262fe69d7b47015eded2a35067550
gwitcig2 <gwitcig2@huskers.unl.edu>
2025-10-03 13:01:04 -0500

    Updated vite config and tsconfig files so they recognize the new shared directory

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
   ],
   resolve: {
       alias: {
-          "@": path.resolve(__dirname, "./src"),
+          "@": path.resolve(__dirname, "./client/src"),
+          "@shared": path.resolve(__dirname, "./shared"),
       },
   },
 })
