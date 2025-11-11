da3204e1c4164fd0c80ed439ba2d204fb73387f0
gwitcig2 <gwitcig2@huskers.unl.edu>
2025-11-10 19:07:32 -0600

    /api: Modified env-config to throw an error instead of using process.exit()

--- apps/api/src/config/env-config.js
+++ apps/api/src/config/env-config.js
@@ -3,9 +3,9 @@ import { apiEnvSchema } from '@kanpeki/env-vars';
 const parseProcessEnv = apiEnvSchema.safeParse(process.env);
 
 if (!parseProcessEnv.success) {
-    console.error("The .env variables for /api are invalid:");
-    console.error(parseProcessEnv.error.issues);
-    process.exit(1);
+    console.log("The .env variables for /api are invalid:");
+    console.log(parseProcessEnv.error.issues);
+    throw new Error(parseProcessEnv.error.message);
 }
 
 export const env = parseProcessEnv.data;
